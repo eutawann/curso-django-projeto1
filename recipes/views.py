694b15ff30367ab07689982c79eb540480278d63
Tawan de Sousa Carvalho <principalacc169@gmail.com>
2025-10-05 17:51:34 -0300

    fix: removendo exemplos

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # HTTP REQUEST
 def home(request):
@@ -7,9 +6,3 @@ def home(request):
         'name': 'Tawan Carvalho'
     })
     # return HTTP response
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse("SOBRE")
