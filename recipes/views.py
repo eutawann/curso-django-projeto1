858d111d605cd2ec225666500f9f39e9eea4c006
Tawan de Sousa Carvalho <principalacc169@gmail.com>
2025-10-07 00:46:06 -0300

    pronto para iniciar django models

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # HTTP REQUEST
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Tawan Carvalho'
     })
-    # return HTTP response
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Tawan Carvalho'
+    })
+
